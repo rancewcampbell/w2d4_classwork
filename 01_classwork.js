c88986670fdd28a46fe9aff119fc4c40ea984a15
Rance Campbell <rancewcampbell@gmail.com>
2020-04-09 16:56:11 +0000

    01.classwork.js loops through data and logs contents of files

--- 01_classwork.js
+++ 01_classwork.js
@@ -2,3 +2,14 @@ const fs = require('fs');
 
 // Problem: Read the data files ** in order **, creating one final string as a result
 // The data files are in the ./data directory
+
+fs.readdir('./data', 'utf8', (err, files) => {
+  if (err) throw err;
+  for (const file of files) {
+    fs.readFile(`./data/${file}`, 'utf8', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    })
+  }
+});
+
